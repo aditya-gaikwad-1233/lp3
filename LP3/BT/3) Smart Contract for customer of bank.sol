33ebf9185fd16d6daae7ffac1a7d668dd7e0c21a
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-11-07 15:29:49 +0530

    Update 3) Smart Contract for customer of bank.sol

--- LP3/BT/3) Smart Contract for customer of bank.sol
+++ LP3/BT/3) Smart Contract for customer of bank.sol
@@ -2,3 +2,39 @@
 # • Deposit money 
 # • Withdraw Money 
 # • Show balance
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BankAccount {
+    address public owner;
+    mapping(address => uint256) balances;
+
+    constructor() {
+        owner = msg.sender;
+    }                                                                                                                                                                                                                                                                       
+                                                                          
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this operation");
+        _;
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+
+    function getContractBalance() public view onlyOwner returns (uint256) {
+        return address(this).balance;
+    }
+}
