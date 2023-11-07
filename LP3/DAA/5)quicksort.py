7f83ec0a0cfadfba93ceade1dcc617959b95f2bb
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-11-07 15:12:59 +0530

    Update 5)quicksort.py

--- LP3/DAA/5)quicksort.py
+++ LP3/DAA/5)quicksort.py
@@ -1,50 +1,45 @@
 Write a program for analysis of quick sort by using deterministic and randomized variant.
 import random
-import time
+import timeit
 
-def deterministic_quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+def deterministic_partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
 
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
 
-    return deterministic_quick_sort(left) + middle + deterministic_quick_sort(right)
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
-def randomized_quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+def randomized_partition(arr, low, high):
+    pivot_index = random.randint(low, high)
+    arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+    return deterministic_partition(arr, low, high)
 
-    pivot = random.choice(arr)
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-
-    return randomized_quick_sort(left) + middle + randomized_quick_sort(right)
-
-def analyze_quick_sort(arr, variant):
-    start_time = time.time()
-    if variant == "deterministic":
-        sorted_arr = deterministic_quick_sort(arr)
-    elif variant == "randomized":
-        sorted_arr = randomized_quick_sort(arr)
-    end_time = time.time()
-
-    execution_time = end_time - start_time
-
-    return sorted_arr, execution_time
+def quick_sort(arr, low, high, pivot_selector):
+    if low < high:
+        pivot_index = pivot_selector(arr, low, high)
+        quick_sort(arr, low, pivot_index - 1, pivot_selector)
+        quick_sort(arr, pivot_index + 1, high, pivot_selector)
 
 if __name__ == "__main__":
-    arr = [5, 2, 9, 3, 6, 8, 1, 7]
+    arr_sizes = [100, 1000, 10000, 100000]
+    for size in arr_sizes:
+        arr = [random.randint(1, 1000) for _ in range(size)]
+        arr.sort(reverse=True)
 
-    # Analyze the deterministic variant
-    sorted_arr_deterministic, time_deterministic = analyze_quick_sort(arr.copy(), "deterministic")
-    print(f"Deterministic Variant Sorted Array: {sorted_arr_deterministic}")
-    print(f"Deterministic Variant Execution Time: {time_deterministic} seconds")
-
-    # Analyze the randomized variant
-    sorted_arr_randomized, time_randomized = analyze_quick_sort(arr.copy(), "randomized")
-    print(f"Randomized Variant Sorted Array: {sorted_arr_randomized}")
-    print(f"Randomized Variant Execution Time: {time_randomized} seconds")
+        deterministic_time = timeit.timeit("quick_sort(arr.copy(), 0, len(arr) - 1, deterministic_partition)",
+                                        globals=globals(),
+                                        number=10)
+        
+        randomized_time = timeit.timeit("quick_sort(arr.copy(), 0, len(arr) - 1, randomized_partition)",
+                                        globals=globals(),
+                                        number=10)
+        
+        print(f"Array size: {size}")
+        print(f"Deterministic Quick Sort time: {deterministic_time:.6f} seconds")
+        print(f"Randomized Quick Sort time: {randomized_time:.6f} seconds")
+        print("-" * 40)
