16416652881c37cb2a3db07f8940b6effce1a85b
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-11-07 14:35:02 +0530

    Update 5)quicksort.java

--- LP3/DAA/5)quicksort.java
+++ LP3/DAA/5)quicksort.java
@@ -1,74 +1,65 @@
 #Write a program for analysis of quick sort by using deterministic and randomized variant
-package com.JournalDev;
-public class Main {
-static final int N = 4;
+import java.util.Arrays;
+import java.util.Random;
 
-// print the final solution matrix
-static void printSolution(int board[][])
-{
-for (int i = 0; i &lt; N; i++) {
-for (int j = 0; j &lt; N; j++)
-System.out.print(&quot; &quot; + board[i][j]
-+ &quot; &quot;);
-System.out.println();
-}
-}
+public class QuickSortAnalysis {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 3, 6, 8, 1, 7};
 
-// function to check whether the position is safe or not
-static boolean isSafe(int board[][], int row, int col)
-{
-int i, j;
-for (i = 0; i &lt; col; i++)
-if (board[row][i] == 1)
-return false;
+        // Analyze the deterministic variant
+        long startTimeDeterministic = System.nanoTime();
+        deterministicQuickSort(arr.clone());
+        long endTimeDeterministic = System.nanoTime();
+        long executionTimeDeterministic = endTimeDeterministic - startTimeDeterministic;
 
-for (i = row, j = col; i &gt;= 0 &amp;&amp; j &gt;= 0; i--, j--)
+        System.out.println("Deterministic Variant Sorted Array: " + Arrays.toString(arr));
+        System.out.println("Deterministic Variant Execution Time: " + executionTimeDeterministic + " nanoseconds");
 
-if (board[i][j] == 1)
-return false;
+        // Analyze the randomized variant
+        long startTimeRandomized = System.nanoTime();
+        randomizedQuickSort(arr.clone());
+        long endTimeRandomized = System.nanoTime();
+        long executionTimeRandomized = endTimeRandomized - startTimeRandomized;
 
-for (i = row, j = col; j &gt;= 0 &amp;&amp; i &lt; N; i++, j--)
-if (board[i][j] == 1)
-return false;
+        System.out.println("Randomized Variant Sorted Array: " + Arrays.toString(arr));
+        System.out.println("Randomized Variant Execution Time: " + executionTimeRandomized + " nanoseconds");
+    }
 
-return true;
-}
+    public static void deterministicQuickSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
 
-// The function that solves the problem using backtracking
-public static boolean solveNQueen(int board[][], int col)
-{
-if (col &gt;= N)
-return true;
+        int pivot = arr[arr.length / 2];
+        int[] left = Arrays.stream(arr).filter(x -> x < pivot).toArray();
+        int[] middle = Arrays.stream(arr).filter(x -> x == pivot).toArray();
+        int[] right = Arrays.stream(arr).filter(x -> x > pivot).toArray();
 
-for (int i = 0; i &lt; N; i++) {
-//if it is safe to place the queen at position i,col -&gt; place it
-if (isSafe(board, i, col)) {
-board[i][col] = 1;
+        deterministicQuickSort(left);
+        deterministicQuickSort(right);
 
-if (solveNQueen(board, col + 1))
-return true;
+        System.arraycopy(left, 0, arr, 0, left.length);
+        System.arraycopy(middle, 0, arr, left.length, middle.length);
+        System.arraycopy(right, 0, arr, left.length + middle.length, right.length);
+    }
 
-//backtrack if the above condition is false
-board[i][col] = 0;
-}
+    public static void randomizedQuickSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
 
-}
-return false;
-}
+        int pivotIndex = new Random().nextInt(arr.length);
+        int pivot = arr[pivotIndex];
 
-public static void main(String args[])
-{
-int board[][] = { { 0, 0, 0, 0 },
-{ 0, 0, 0, 0 },
-{ 0, 0, 0, 0 },
-{ 0, 0, 0, 0 } };
+        int[] left = Arrays.stream(arr).filter(x -> x < pivot).toArray();
+        int[] middle = Arrays.stream(arr).filter(x -> x == pivot).toArray();
+        int[] right = Arrays.stream(arr).filter(x -> x > pivot).toArray();
 
-if (!solveNQueen(board, 0)) {
-System.out.print(&quot;Solution does not exist&quot;);
-return;
-}
+        randomizedQuickSort(left);
+        randomizedQuickSort(right);
 
-printSolution(board);
-
-}
+        System.arraycopy(left, 0, arr, 0, left.length);
+        System.arraycopy(middle, 0, arr, left.length, middle.length);
+        System.arraycopy(right, 0, arr, left.length + middle.length, right.length);
+    }
 }
