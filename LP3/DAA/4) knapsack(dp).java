17b914ce79a696fe0a886502cc1235a9d797239b
aditya-gaikwad-1233 <114388065+aditya-gaikwad-1233@users.noreply.github.com>
2024-11-13 01:21:53 +0530

    Update 4) knapsack(dp).java

--- LP3/DAA/4) knapsack(dp).java
+++ LP3/DAA/4) knapsack(dp).java
@@ -1,66 +1,54 @@
-#Write a program to solve a 0-1 Knapsack problem using dynamic programming strategy.
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-public class Zero_One_Knapsack
-{
-public void solve(int[] wt, int[] val, int W, int N)
-{
-int NEGATIVE_INFINITY = Integer.MIN_VALUE;
-int[][] m = new int[N + 1][W + 1];
-int[][] sol = new int[N + 1][W + 1];
-for (int i = 1; i &lt;= N; i++)
-{
-for (int j = 0; j &lt;= W; j++)
-{
-int m1 = m[i - 1][j];
-int m2 = NEGATIVE_INFINITY;
-if (j &gt;= wt[i])
-m2 = m[i - 1][j - wt[i]] + val[i];
-m[i][j] = Math.max(m1, m2);
-sol[i][j] = m2 &gt; m1 ? 1 : 0;
+class Item {
+    int weight;
+    int value;
+
+    Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+
+    double valuePerWeight() {
+        return (double) value / weight;
+    }
 }
-}
-int[] selected = new int[N + 1];
-for (int n = N, w = W; n &gt; 0; n--)
-{
-if (sol[n][w] != 0)
 
-{
-selected[n] = 1;
-w = w - wt[n];
-}
-else
-selected[n] = 0;
-}
-System.out.print(&quot;\nItems with weight &quot;);
-for (int i = 1; i &lt; N + 1; i++)
-if (selected[i] == 1)
-System.out.print(val[i] +&quot; &quot;);
-System.out.println(&quot;are selected by knapsack algorithm.&quot;);
-}
-public static void main (String[] args)
-{
-Scanner scan = new Scanner(System.in);
-Zero_One_Knapsack ks = new Zero_One_Knapsack();
+class Main {
 
-System.out.println(&quot;Enter number of elements &quot;);
-int n = scan.nextInt();
+    public static void fractionalKnapsack() {
+        int[] weights = {10, 20, 30};
+        int[] values = {60, 100, 120};
+        int capacity = 50;
+        double res = 0.0;
 
-int[] wt = new int[n + 1];
-int[] val = new int[n + 1];
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < weights.length; i++) {
+            items.add(new Item(weights[i], values[i]));
+        }
 
-System.out.println(&quot;Enter weight for &quot;+ n +&quot; elements&quot;);
-for (int i = 1; i &lt;= n; i++)
-wt[i] = scan.nextInt();
+        items.sort(Comparator.comparingDouble(Item::valuePerWeight).reversed());
 
-System.out.println(&quot;Enter value for &quot;+ n +&quot; elements&quot;);
-for (int i = 1; i &lt;= n; i++)
-val[i] = scan.nextInt();
+        for (Item item : items) {
+            if (capacity <= 0) {
+                break;
+            }
+            if (item.weight > capacity) {
+                res += capacity * item.valuePerWeight();
+                capacity = 0;
+            } else {
+                res += item.value;
+                capacity -= item.weight;
+            }
+        }
 
-System.out.println(&quot;Enter knapsack weight &quot;);
-int W = scan.nextInt();
+        System.out.println((int) res);
+    }
 
-ks.solve(wt, val, W, n);
-scan.close();
-}
+    public static void main(String[] args) {
+        fractionalKnapsack();
+    }
 }
