1c0e2ea66f1e0c4b87d0e475506e2ef77f5d9b13
aditya-gaikwad-1233 <114388065+aditya-gaikwad-1233@users.noreply.github.com>
2024-11-13 01:20:53 +0530

    Update 3)fractionalknapsack.java

--- LP3/DAA/3)fractionalknapsack.java
+++ LP3/DAA/3)fractionalknapsack.java
@@ -1,57 +1,54 @@
-#Write a program to solve a fractional Knapsack problem using a greedy method.
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-class Fractional_Knapsack
-{
-public static void main(String args[]) throws IOException
-{
-int i,j=0,max_qty,m,n;
-float sum=0,max;
-Scanner sc = new Scanner(System.in);
-int array[][]=new int[2][20];
-System.out.println(&quot;Enter no of items&quot;);
-n=sc.nextInt();
+class Item {
+    int weight;
+    int value;
 
-System.out.println(&quot;Enter the weights of each items&quot;);
-for(i=0;i&lt;n;i++)
-array[0][i]=sc.nextInt();
+    Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
 
-System.out.println(&quot;Enter the values of each items&quot;);
-for(i=0;i&lt;n;i++)
-array[1][i]=sc.nextInt();
+    double valuePerWeight() {
+        return (double) value / weight;
+    }
+}
 
-System.out.println(&quot;Enter maximum volume of knapsack :&quot;);
-max_qty=sc.nextInt();
+class Main {
 
-m=max_qty;
-while(m&gt;=0)
-{
-max=0;
-for(i=0;i&lt;n;i++)
-{
-if(((float)array[1][i])/((float)array[0][i])&gt;max)
-{
-max=((float)array[1][i])/((float)array[0][i]);
-j=i;
-}
-}
-if(array[0][j]&gt;m)
-{
-System.out.println(&quot;Quantity of item number: &quot; + (j+1) + &quot; added is &quot; +m);
-sum+=m*max;
-m=-1;
-}
-else
-{
-System.out.println(&quot;Quantity of item number: &quot; + (j+1) + &quot; added is &quot; + array[0][j]);
-m-=array[0][j];
-sum+=(float)array[1][j];
-array[1][j]=0;
-}
-}
-System.out.println(&quot;The total profit is &quot; + sum);
+    public static void fractionalKnapsack() {
+        int[] weights = {10, 20, 30};
+        int[] values = {60, 100, 120};
+        int capacity = 50;
+        double res = 0.0;
 
-sc.close();
-}
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < weights.length; i++) {
+            items.add(new Item(weights[i], values[i]));
+        }
+
+        items.sort(Comparator.comparingDouble(Item::valuePerWeight).reversed());
+
+        for (Item item : items) {
+            if (capacity <= 0) {
+                break;
+            }
+            if (item.weight > capacity) {
+                res += capacity * item.valuePerWeight();
+                capacity = 0;
+            } else {
+                res += item.value;
+                capacity -= item.weight;
+            }
+        }
+
+        System.out.println((int) res);
+    }
+
+    public static void main(String[] args) {
+        fractionalKnapsack();
+    }
 }
