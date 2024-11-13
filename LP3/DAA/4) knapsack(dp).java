c7bc7711bd9333fe3af9e03dc5d6ab6a43b742ac
aditya-gaikwad-1233 <114388065+aditya-gaikwad-1233@users.noreply.github.com>
2024-11-13 07:23:28 +0530

    Update 4) knapsack(dp).java

--- LP3/DAA/4) knapsack(dp).java
+++ LP3/DAA/4) knapsack(dp).java
@@ -1,54 +1,48 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-
-class Item {
-    int weight;
-    int value;
-
-    Item(int weight, int value) {
-        this.weight = weight;
-        this.value = value;
-    }
-
-    double valuePerWeight() {
-        return (double) value / weight;
-    }
-}
 
 class Main {
 
-    public static void fractionalKnapsack() {
-        int[] weights = {10, 20, 30};
-        int[] values = {60, 100, 120};
-        int capacity = 50;
-        double res = 0.0;
+    public static int knapsack0_1(int[] values, int[] weights, int capacity) {
+        int n = values.length;
+        int[][] dp = new int[n + 1][capacity + 1];
 
-        List<Item> items = new ArrayList<>();
-        for (int i = 0; i < weights.length; i++) {
-            items.add(new Item(weights[i], values[i]));
-        }
-
-        items.sort(Comparator.comparingDouble(Item::valuePerWeight).reversed());
-
-        for (Item item : items) {
-            if (capacity <= 0) {
-                break;
-            }
-            if (item.weight > capacity) {
-                res += capacity * item.valuePerWeight();
-                capacity = 0;
-            } else {
-                res += item.value;
-                capacity -= item.weight;
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
             }
         }
 
-        System.out.println((int) res);
+        List<Integer> selectedItems = new ArrayList<>();
+        int i = n, j = capacity;
+        while (i > 0 && j > 0) {
+            if (dp[i][j] != dp[i - 1][j]) {
+                selectedItems.add(i - 1);
+                j -= weights[i - 1];
+            }
+            i--;
+        }
+
+        System.out.println("Maximum value: " + dp[n][capacity]);
+        System.out.print("Selected items: ");
+        for (int item : selectedItems) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        return dp[n][capacity];
     }
 
     public static void main(String[] args) {
-        fractionalKnapsack();
+        int[] values = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int capacity = 50;
+        knapsack0_1(values, weights, capacity);
     }
 }
